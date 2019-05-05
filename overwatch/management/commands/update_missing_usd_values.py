b7a4bf787f90b6a74755f2dc2dec5cf6c197830a
inuitwallet <mj2p.sg@gmail.com>
2019-05-05 21:10:18 +0100

    add management script to update usd values where missing

--- overwatch/management/commands/update_missing_usd_values.py
+++ overwatch/management/commands/update_missing_usd_values.py
@@ -1,9 +1,25 @@
 from django.core.management import BaseCommand
 
+from overwatch.models import BotBalance, BotPlacedOrder, BotPrice, BotTrade
+
 
 class Command(BaseCommand):
     """
-    If there are
+    If there are missing usd calculations we can update them all by passing objects to the consumers
     """
     def handle(self, *args, **options):
-        pass
+        # bot_balance
+        for balance in BotBalance.objects.filter(updated=False):
+            balance.save()
+
+        # bot_price
+        for price in BotPrice.objects.filter(updated=False):
+            price.save()
+
+        # bot_order
+        for order in BotPlacedOrder.objects.filter(updated=False):
+            order.save()
+
+        # bot_trade
+        for trade in BotTrade.objects.filter(updated=False):
+            trade.save()
