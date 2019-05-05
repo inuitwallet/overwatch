b121c2042532cb08526c491e60684f574bcfa8af
inuitwallet <mj2p.sg@gmail.com>
2019-05-05 21:15:17 +0100

    add logging

--- overwatch/management/commands/update_missing_usd_values.py
+++ overwatch/management/commands/update_missing_usd_values.py
@@ -1,3 +1,5 @@
+import logging
+
 from django.core.management import BaseCommand
 
 from overwatch.models import BotBalance, BotPlacedOrder, BotPrice, BotTrade
@@ -7,19 +9,33 @@ class Command(BaseCommand):
     """
     If there are missing usd calculations we can update them all by passing objects to the consumers
     """
+    log = logging.getLogger(__name__)
+
     def handle(self, *args, **options):
         # bot_balance
+        self.log.info('Processing BotBalances')
+
         for balance in BotBalance.objects.filter(updated=False):
+            self.log.info(balance)
             balance.save()
 
         # bot_price
+        self.log.info('Processing BotPrices')
+
         for price in BotPrice.objects.filter(updated=False):
+            self.log.info(price)
             price.save()
 
         # bot_order
+        self.log.info('Processing BotPlacedOrders')
+
         for order in BotPlacedOrder.objects.filter(updated=False):
+            self.log.info(order)
             order.save()
 
         # bot_trade
+        self.log.info('Processing BotTrades')
+
         for trade in BotTrade.objects.filter(updated=False):
+            self.log.info(trade)
             trade.save()
