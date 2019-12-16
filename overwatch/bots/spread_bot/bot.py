6ad469cd0a60d99d57e7de4b1ba670a29b7f6330
inuitwallet <mj2p.sg@gmail.com>
2019-12-16 21:22:22 +0000

    reverse the date sort on my trades

--- overwatch/bots/spread_bot/bot.py
+++ overwatch/bots/spread_bot/bot.py
@@ -617,7 +617,7 @@ class Bot(object):
 
         self.logger.info('Got Last trade id : {}'.format(last_trade_id))
 
-        for trade in self.wrapper.fetch_my_trades(self.symbol):
+        for trade in sorted(b.fetch_my_trades('ARK/BTC'), key=lambda x: x['datetime'], reverse=True):
             self.logger.info('Found Trade with ID: {}'.format(trade.get('id')))
 
             if trade.get('id') == last_trade_id:
